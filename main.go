8896fea45da247216bac5fd3091a950f81023d56
Ildar Karymov <hi@ildarkarymov.ru>
2025-02-05 00:57:58 +0100

    Add simple REPL

--- main.go
+++ main.go
@@ -2,24 +2,52 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io"
+	"os"
+	"strings"
 
+	"github.com/chzyer/readline"
 	"github.com/defer-panic/dumbql/query"
 )
 
 func main() {
-	// Example query string.
-	// You can try different inputs, for example:
-	// input := `status:200`
-	// input := `NOT error`
-	// input := `status:200 AND (extension:"jpg" OR extension:"png")`
-	input := `status : 200 and eps < 0.003 and (req.fields.ext:["jpg", "png"])`
-	fmt.Printf("input: %s\n", input)
-
-	ast, err := query.Parse("query", []byte(input))
+	rl, err := readline.New("> ")
 	if err != nil {
-		log.Fatalf("Error parsing query: %s", err)
+		fmt.Fprintf(os.Stderr, "Error initializing readline: %v\n", err)
+		os.Exit(1)
 	}
-	// fmt.Printf("AST:\n%#v\n", ast.(query.Expr).String())
-	fmt.Println(ast.(query.Expr))
+	defer rl.Close()
+
+	fmt.Println("DumbQL (c) defer panic Inc. 2077")
+	fmt.Println("Enter your queries below (type 'exit' or 'quit' to leave).")
+
+	for {
+		line, err := rl.Readline()
+		if err == readline.ErrInterrupt {
+			if len(line) == 0 {
+				break
+			}
+			continue
+		} else if err == io.EOF {
+			break
+		}
+
+		trimmed := strings.TrimSpace(line)
+		if trimmed == "" {
+			continue
+		}
+		if strings.EqualFold(trimmed, "exit") || strings.EqualFold(trimmed, "quit") {
+			break
+		}
+
+		ast, err := query.Parse("stdin", []byte(trimmed))
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			continue
+		}
+
+		fmt.Println(ast.(query.Expr))
+	}
+
+	fmt.Println("Goodbye!")
 }
