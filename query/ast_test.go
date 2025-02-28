b07e6c8ba859c66d961f5f74348138062a9e648a
Ildar Karymov <hi@ildarkarymov.ru>
2025-02-28 17:52:45 +0100

    Add tests for boolean values
    - Test BoolLiteral.Value() method
    - Test SQL generation for boolean values
    - Test shorthand syntax SQL generation

---
+++ query/ast_test.go
@@ -0,0 +1,33 @@
+package query_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.tomakado.io/dumbql/query"
+)
+
+func TestLiteralValues(t *testing.T) {
+	t.Run("StringLiteral.Value", func(t *testing.T) {
+		sl := &query.StringLiteral{StringValue: "hello"}
+		assert.Equal(t, "hello", sl.Value())
+	})
+
+	t.Run("NumberLiteral.Value", func(t *testing.T) {
+		nl := &query.NumberLiteral{NumberValue: 42.5}
+		assert.Equal(t, 42.5, nl.Value())
+	})
+
+	t.Run("BoolLiteral.Value", func(t *testing.T) {
+		bl1 := &query.BoolLiteral{BoolValue: true}
+		assert.Equal(t, true, bl1.Value())
+
+		bl2 := &query.BoolLiteral{BoolValue: false}
+		assert.Equal(t, false, bl2.Value())
+	})
+
+	t.Run("Identifier.Value", func(t *testing.T) {
+		id := query.Identifier("field")
+		assert.Equal(t, "field", id.Value())
+	})
+}
