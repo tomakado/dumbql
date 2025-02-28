e2fd8b7cd2fffc97bbe38aa03e2981cee667284e
Ildar Karymov <hi@ildarkarymov.ru>
2025-02-28 17:48:34 +0100

    Fix example test and add linting directive

--- dumbql_example_test.go
+++ dumbql_example_test.go
@@ -78,6 +78,6 @@ func ExampleParse_booleanFields() {
 	}
 
 	fmt.Println(ast)
-	// Output: (and (and (and (= verified true) (= premium true)) (not (= banned true)))
-	//          (or (= admin true) (= moderator true)))
+	//nolint:lll
+	// Output: (and (and (and (= verified true) (= premium true)) (not (= banned true))) (or (= admin true) (= moderator true)))
 }
